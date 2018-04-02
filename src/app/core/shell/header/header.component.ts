0e8e0ca8bca926b343ee414d468baea5f9ecf360
Daniel Newell <daniel.newell@amway.com>
2018-04-02 15:12:18 -0400

    Initial Commit

---
+++ src/app/core/shell/header/header.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from '../../authentication/authentication.service';
+import { I18nService } from '../../i18n.service';
+
+const NAVIGATION_LINKS = [
+  {
+    text: 'Home',
+    route: '/home'
+  },
+  {
+    text: 'About',
+    route: '/about'
+  }
+];
+
+@Component({
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss']
+})
+export class HeaderComponent implements OnInit {
+  navigationLinks = {};
+  menuHidden = true;
+
+  constructor(private router: Router,
+              private authenticationService: AuthenticationService,
+              private i18nService: I18nService) { }
+
+  ngOnInit() {
+    this.navigationLinks = NAVIGATION_LINKS;
+  }
+
+  toggleMenu() {
+    this.menuHidden = !this.menuHidden;
+  }
+
+  setLanguage(language: string) {
+    this.i18nService.language = language;
+  }
+
+  logout() {
+    this.authenticationService.logout()
+      .subscribe(() => this.router.navigate(['/login'], { replaceUrl: true }));
+  }
+
+  get currentLanguage(): string {
+    return this.i18nService.language;
+  }
+
+  get languages(): string[] {
+    return this.i18nService.supportedLanguages;
+  }
+
+  get username(): string | null {
+    const credentials = this.authenticationService.credentials;
+    return credentials ? credentials.username : null;
+  }
+
+}
